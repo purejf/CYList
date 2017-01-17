c0d893787abea59d6c85460b8f68ac7c048697f1
姚鑫 <charlesyaoxin@163.com>
2017-01-17 18:33:45 +0800

    first commit

---
+++ LB/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  LB
+//
+//  Created by Charles on 17/1/17.
+//  Copyright © 2017年 Charles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ LB/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  LB
+//
+//  Created by Charles on 17/1/17.
+//  Copyright © 2017年 Charles. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
