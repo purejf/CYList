065b10fc13d968d8ae6e5c142375c6cf836b908b
姚鑫 <charlesyaoxin@163.com>
2017-01-17 15:48:05 +0800

    Initial Commit

---
+++ LB/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LB
+//
+//  Created by Charles on 17/1/17.
+//  Copyright © 2017年 Charles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
